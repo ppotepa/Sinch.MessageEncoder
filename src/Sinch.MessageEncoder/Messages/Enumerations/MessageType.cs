4f82462e6d9a3a15d79f22eaecb3d64baaf0d370
Pawel.Potepa <pawel.potepa@hotmail.com>
2023-05-17 09:37:25 +0200

    Fix FileNaming Add Basic Summary

---
+++ src/Sinch.MessageEncoder/Messages/Enumerations/MessageType.cs
@@ -0,0 +1,13 @@
+﻿namespace Sinch.MessageEncoder.Messages.Enumerations
+{
+    /// <summary>
+    /// Each Enumeration here represents single <see cref="byte"/> block of memory
+    /// </summary>
+    public enum MessageType
+    {
+        Text = 1,
+        Voice = 2,
+        Image = 3,
+        Video = 4
+    }
+}
