1fe6b0f99d37764af6913043d5e4bff1a181ca02
Pawel.Potepa <pawel.potepa@hotmail.com>
2023-05-30 11:26:33 +0200

    Refactor bits of code

--- src/Sinch.MessageEncoder.Tests/BinarySerializerTests.cs
+++ src/Sinch.MessageEncoder.Tests/BinarySerializerTests.cs
@@ -17,18 +17,10 @@ namespace Sinch.MessageEncoder.Tests
                 1,   0, 0,   0,   0, 0, 0, 0,
                 2,   0, 0,   0,   0, 0, 0, 0,
                 134, 1, 114, 100, 0, 0, 0, 0,
-                1,
-                //2, 100, 0,
-                //2, 16, 39,
-                //4, 0, 202, 154, 59,
-                //8, 0, 16, 165, 212, 232, 0, 0, 0
+                1
             };
 
             byte[] binaryMessageBuilder = new BinaryMessageBuilder((long)1, (long)2, (long)1685193094, (byte)1)
-                //.AddHeader("byte", (byte)100)
-                //.AddHeader("short", (short)10_000)
-                //.AddHeader("int", (int)1_000_000_000)
-                //.AddHeader("long", (long)1_000_000_000_000)
                 .Serialize();
 
             Assert.IsTrue(assertBytes.SequenceEqual(binaryMessageBuilder)); ;
@@ -65,17 +57,17 @@ namespace Sinch.MessageEncoder.Tests
                 (short)8, (long)100,
                 (short)32, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
             };
-            IEnumerable<byte> defaultSerializerResult = defaultSerializerInput.SelectMany(integer => integer.ToByteArray());
 
+            IEnumerable<byte> defaultSerializerResult = defaultSerializerInput.SelectMany(integer => integer.ToByteArray());
             IEnumerable<byte> binarySerializerResult = defaultSerializerInput.SelectMany(data =>
             {
                 var result = data switch
                 {
                     //out of some reason BitConverter interprets single byte as 2-byte digit ?? weird
                     byte @byte => BitConverter.GetBytes(@byte).Take(1),
-                    short @short => BitConverter.GetBytes(@short).Take(2),
-                    int @int => BitConverter.GetBytes(@int).Take(4),
-                    long @long => BitConverter.GetBytes(@long).Take(8),
+                    short @short => BitConverter.GetBytes(@short),
+                    int @int => BitConverter.GetBytes(@int),
+                    long @long => BitConverter.GetBytes(@long),
                     string @string => System.Text.Encoding.ASCII.GetBytes(@string),
                     _ => throw new ArgumentOutOfRangeException(nameof(data), data, null)
                 };
@@ -101,7 +93,7 @@ namespace Sinch.MessageEncoder.Tests
         }
 
         [Test]
-        public void My_Stupid_BitConversion_Works()
+        public void Custom_Bit_Converter_Matches_Results_With_Built_In_One()
         {
             Assert.IsTrue(BitConverter.GetBytes(1).SequenceEqual(1.ToByteArray()));
             Assert.IsTrue(BitConverter.GetBytes(1_000).SequenceEqual(1_000.ToByteArray()));
